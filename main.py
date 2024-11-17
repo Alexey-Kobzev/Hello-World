f25434b735ba939483da3937bbae45a5e9545225
Alexey-Kobzev <alex20vek1998@gmail.com>
2024-11-17 12:45:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hello World")  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+    print("Hello World")  
 if __name__ == '__main__':
     print_hi('PyCharm')
